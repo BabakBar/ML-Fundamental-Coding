f353d77567a748a3ea6d663194e64ea6517520b7
BabakBar <babak.barghi@gmail.com>
2022-03-21 10:00:26 +0100

    clean the final code

--- Day1- Life in Weeks/Life_in_Weeks.py
+++ Day1- Life in Weeks/Life_in_Weeks.py
@@ -1,6 +1,6 @@
 age = int(input("Your current age"))
-months = 12*(70 - age)
-weeks = 52*(70 - age)
-days = 365*(70 - age)
 years = 70 - age
+months = 12*years
+weeks = 52*years
+days = 365*years
 answer = print(f"You have {days} days, {weeks} weeks, {months} months, {years} years left")
