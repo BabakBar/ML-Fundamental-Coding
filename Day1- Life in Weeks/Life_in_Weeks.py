11adfccaa6e26ee9f99b58f7c97e43bddcc76b52
BabakBar <babak.barghi@gmail.com>
2022-03-21 09:57:34 +0100

    life in time calculator

--- Day1- Life in Weeks/Life_in_Weeks.py
+++ Day1- Life in Weeks/Life_in_Weeks.py
@@ -2,4 +2,5 @@ age = int(input("Your current age"))
 months = 12*(70 - age)
 weeks = 52*(70 - age)
 days = 365*(70 - age)
-answer = (f"You have {days} days, {weeks} weeks, and {months} months left")
+years = 70 - age
+answer = print(f"You have {days} days, {weeks} weeks, {months} months, {years} years left")
