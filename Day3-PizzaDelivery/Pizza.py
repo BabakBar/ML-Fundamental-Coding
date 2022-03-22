e78d70a9a9b2d75b1af61b92fb4bdfd46e756f95
BabakBar <babak.barghi@gmail.com>
2022-03-22 13:26:43 +0100

    first draft of code

--- Day3-PizzaDelivery/Pizza.py
+++ Day3-PizzaDelivery/Pizza.py
@@ -2,3 +2,28 @@ print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M, or L ")
 add_pepperoni = input("Do you want pepperoni? Y or N ")
 extra_cheese = input("Do you want extra cheese? Y or N ")
+bill = 0
+
+size = "M"
+add_pepperoni = "Y"
+extra_cheese = "Y"
+if size == "S":
+    bill = 15
+    if add_pepperoni == "Y":
+        bill += 2
+elif size == "M":
+    bill = 20
+    if add_pepperoni == "Y":
+        bill += 3
+elif size == "L":
+    bill = 25
+    if add_pepperoni == "Y":
+        bill += 3
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"your bill is ${bill}")
+
+
+
+
