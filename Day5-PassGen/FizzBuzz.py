b1b489c9ae42a8340d72f3b6cc1d20cf5f58e3c7
BabakBar <babak.barghi@gmail.com>
2022-04-12 07:23:18 +0200

    try1

---
+++ Day5-PassGen/FizzBuzz.py
@@ -0,0 +1,8 @@
+numbers = (1, 100)
+for num in range(numbers):
+    if num % 3 == 0:
+        print("Fizz")
+    if num % 5 == 0:
+        print("Buzz")
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
