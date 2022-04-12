7db9319332eee5b046db0f88748660ea66507e8c
BabakBar <babak.barghi@gmail.com>
2022-04-12 07:28:29 +0200

    print stm

--- Day5-PassGen/FizzBuzz.py
+++ Day5-PassGen/FizzBuzz.py
@@ -1,9 +1,9 @@
 for num in range(1, 101):
     if num % 3 == 0 and num % 5 == 0:
-        print("Fizz")
+        print("FizzBuzz")
     elif num % 5 == 0:
         print("Buzz")
     elif num % 3 == 0:
-        print("FizzBuzz")
+        print("Fizz")
     else:
         print(num)
